81b70fd33fbf6e3cbe2774a14777289eb10a7566
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-08-28 00:07:29 -0700

    hammerspoon: Normalize config

---
+++ common/platform-osx/hammerspoon/init.lua
@@ -0,0 +1,50 @@
+super = {"cmd", "ctrl", "shift", "alt"}
+
+-- Hide ugly toolbar
+hs.console.toolbar(nil)
+
+-- See https://github.com/Hammerspoon/hammerspoon/issues/595
+windowFilter = hs.window.filter.new():setCurrentSpace(true)
+
+hs.hotkey.bind(super, "\\", function()
+    nextScreen = hs.window.focusedWindow():screen():next()
+
+    wf = hs.window.filter.new()
+    wf:setOverrideFilter({ allowScreens = nextScreen:id() })
+    wf:setSortOrder(hs.window.filter.sortByFocusedLast)
+
+    window = wf:getWindows()[1]
+    window:focus()
+    hs.mouse.setAbsolutePosition(window:frame().center)
+end)
+
+hs.hotkey.bind(super, "o", function()
+    focused = hs.window.focusedWindow()
+    focused:moveToScreen(focused:screen():next())
+
+    focused:focus()
+    hs.mouse.setAbsolutePosition(focused:frame().center)
+end)
+
+snapWindow = function(window, gridSize)
+    frame = window:frame()
+
+    for _, axis in ipairs({'x', 'x2', 'y', 'y2'}) do
+        frame[axis] = math.floor((frame[axis] / gridSize) + 0.5) * gridSize
+    end
+
+    hs.window.focusedWindow():setFrameInScreenBounds(frame)
+end
+
+hs.hotkey.bind(super, "a", function()
+    snapWindow(hs.window.focusedWindow(), 10)
+end)
+
+hs.hotkey.bind(super, "m", function()
+    hs.window.focusedWindow():centerOnScreen()
+    snapWindow(hs.window.focusedWindow(), 10)
+end)
+
+hs.hotkey.bind(super, "`", nil, function()
+    hs.execute('pmset displaysleepnow')
+end)
