1bd19230587a8babe1148194bbdb20039c9e30e2
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-09 23:05:40 -0700

    vim: Fix lsp things for latest neovim

--- base/nvim/lua/my/configs/lsp/handlers.lua
+++ base/nvim/lua/my/configs/lsp/handlers.lua
@@ -88,13 +88,13 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
   if client.name == "jsonls" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
   if client.name == "html" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
   lsp_keymaps(bufnr)
 end
--- base/nvim/lua/my/configs/lsp/init.lua
+++ base/nvim/lua/my/configs/lsp/init.lua
@@ -1,17 +1,24 @@
-local lspconfig = safe_require("lspconfig")
-if not lspconfig then
-  return
+local M = {}
+
+function M.setup()
+  require("my.configs.lsp.installer").setup()
+
+  local lspconfig = safe_require("lspconfig")
+  if not lspconfig then
+    return
+  end
+
+  -- Add rounded window borders
+  local lspconfig_window = require("lspconfig.ui.windows")
+  local old_defaults = lspconfig_window.default_opts
+
+  function lspconfig_window.default_opts(opts)
+    local win_opts = old_defaults(opts)
+    win_opts.border = "rounded"
+    return win_opts
+  end
+
+  require("my.configs.lsp.handlers").setup()
 end
 
--- Add rounded window borders
-local lspconfig_window = require("lspconfig.ui.windows")
-local old_defaults = lspconfig_window.default_opts
-
-function lspconfig_window.default_opts(opts)
-  local win_opts = old_defaults(opts)
-  win_opts.border = "rounded"
-  return win_opts
-end
-
-require("my.configs.lsp.installer").setup()
-require("my.configs.lsp.handlers").setup()
+return M
