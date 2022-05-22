9f1b4922441a253e597cd6732bd4af4858907c26
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-22 02:46:58 -0700

    vim: Better lualine

--- base/nvim/lua/my/configs/lualine.lua
+++ base/nvim/lua/my/configs/lualine.lua
@@ -6,13 +6,38 @@ function M.setup()
     return
   end
 
+  local function repoName()
+    return " " .. vim.fn.getcwd():match("[^/]*$")
+  end
+
   local config = {
     options = {
       theme = "gruvbox-flat",
 
+      globalstatus = true,
       disabled_filetypes = { "NvimTree", "dashboard", "Outline" },
       component_separators = "",
       section_separators = "",
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { { repoName } },
+      lualine_c = {
+        {
+          "filetype",
+          colored = true,
+          icon_only = true,
+          padding = { left = 1, right = 0 },
+        },
+        {
+          "filename",
+          path = 1,
+          shorting_target = 50,
+        },
+      },
+      lualine_x = { "encoding", "fileformat", "diagnostics" },
+      lualine_y = {},
+      lualine_z = {},
     },
   }
 
