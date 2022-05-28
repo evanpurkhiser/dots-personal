c88a4115f7334195f65378cc4a1d109deb3689dc
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-28 01:04:47 -0700

    macos: Better default keyboard speed

--- common/platform-osx/macos/macos.sh
+++ common/platform-osx/macos/macos.sh
@@ -61,7 +61,7 @@ sudo nvram SystemAudioVolume=" "
 
 # Faster key repeat rate
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 0         # normal minimum is 2 (30 ms)
+defaults write -g KeyRepeat -int 1         # normal minimum is 2 (30 ms)
 
 for app in \
   "cfprefsd" \
