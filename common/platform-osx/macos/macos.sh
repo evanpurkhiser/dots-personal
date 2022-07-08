cef91291cd098540b3b0828ef37c34a82ae276d2
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-07-07 19:13:03 -0700

    macos: Remove unused apps

--- common/platform-osx/macos/macos.sh
+++ common/platform-osx/macos/macos.sh
@@ -63,6 +63,13 @@ sudo nvram SystemAudioVolume=" "
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1         # normal minimum is 2 (30 ms)
 
+# Remove unused apps
+sudo rm -rf /Applications/GarageBand.app
+sudo rm -rf /Applications/Keynote.app
+sudo rm -rf /Applications/Numbers.app
+sudo rm -rf /Applications/Pages.app
+sudo rm -rf /Applications/iMovie.app
+
 for app in \
 	"cfprefsd" \
 	"Dock" \
