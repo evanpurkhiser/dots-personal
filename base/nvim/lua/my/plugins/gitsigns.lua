92986b28d661cc10062a8dc1374a2b4773fd15ef
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-13 22:02:58 -0500

    vim: Add gitsigns

---
+++ base/nvim/lua/my/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+P = {
+  "lewis6991/gitsigns.nvim",
+}
+
+function P.config()
+  require("gitsigns").setup({
+    signs = {
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┃" },
+    },
+  })
+end
+
+return P
