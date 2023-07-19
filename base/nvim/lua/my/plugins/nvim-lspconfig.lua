9a79461c28d4d0c31d8bf33b7f04fca543652584
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-18 21:57:14 -0700

    vim: Improve nvim-lspconfig diagnostic signs

--- base/nvim/lua/my/plugins/nvim-lspconfig.lua
+++ base/nvim/lua/my/plugins/nvim-lspconfig.lua
@@ -62,10 +62,10 @@ function P.config()
   })
 
   local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "󰌵" },
-    { name = "DiagnosticSignInfo", text = "󰋼" },
+    { name = "DiagnosticSignError", text = "━" },
+    { name = "DiagnosticSignWarn", text = "━" },
+    { name = "DiagnosticSignHint", text = "━" },
+    { name = "DiagnosticSignInfo", text = "━" },
   }
 
   for _, sign in ipairs(signs) do
