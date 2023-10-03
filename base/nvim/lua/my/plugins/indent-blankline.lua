21fad090e1f10bb1ea0ddbb613baf74d56137720
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-10-03 14:50:59 -0700

    vim: Update lazy-lock

--- base/nvim/lua/my/plugins/indent-blankline.lua
+++ base/nvim/lua/my/plugins/indent-blankline.lua
@@ -3,19 +3,11 @@ local P = {
   event = "BufRead",
 }
 
-local g = vim.g
-
 function P.config()
-  local indent_blankline = require("indent_blankline")
+  local ibl = require("ibl")
 
-  g.indent_blankline_show_trailing_blankline_indent = false
-  g.indent_blankline_show_first_indent_level = false
-  g.indent_blankline_use_treesitter = true
-  g.indent_blankline_show_current_context = true
-
-  indent_blankline.setup({
-    show_current_context = true,
-    show_current_context_start = false,
+  ibl.setup({
+    indent = { char = "|" },
   })
 end
 
