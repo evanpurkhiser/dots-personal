85972a1720cb548b9abfab66ebf2cb4e5d57e3e9
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-01-14 12:42:13 -0500

    vim: Increase conform formatting timeout

--- base/nvim/lua/my/plugins/conform.lua
+++ base/nvim/lua/my/plugins/conform.lua
@@ -19,7 +19,7 @@ function P.config()
 
     format_on_save = {
       -- These options will be passed to conform.format()
-      timeout_ms = 1000,
+      timeout_ms = 5000,
       lsp_fallback = true,
     },
 
