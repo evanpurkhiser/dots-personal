b6b1bb6efe2130b3548ffd4f99d43cb600597f0d
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-05-07 01:04:36 -0400

    vim: Switch over to more-native lsp config

--- base/nvim/lua/my/plugins/mason.lua
+++ base/nvim/lua/my/plugins/mason.lua
@@ -12,10 +12,29 @@ local P = {
     "MasonUpdate",
     "MasonLog",
   },
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
 }
 
 function P.config()
   require("mason").setup()
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "ansiblels",
+      "bashls",
+      "basedpyright",
+      "cssls",
+      "dockerls",
+      "gopls",
+      "jsonls",
+      "lua_ls",
+      "rust_analyzer",
+      "sqlls",
+      "ts_ls",
+      "yamlls",
+    },
+  })
 end
 
 return P
--- base/nvim/lua/my/plugins/nvim-lspconfig.lua
+++ base/nvim/lua/my/plugins/nvim-lspconfig.lua
@@ -8,81 +8,40 @@ local P = {
 P.dependencies = {
   "hrsh7th/nvim-cmp",
   "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-
   -- Depends on fzf for some lsp keyboard mappings
   "ibhagwan/fzf-lua",
 }
 
 function P.config()
-  local masonLspconfig = require("mason-lspconfig")
-
-  masonLspconfig.setup({
-    ensure_installed = {
-      "ansiblels",
-      "bashls",
-      "basedpyright",
-      "cssls",
-      "dockerls",
-      "gopls",
-      "jsonls",
-      "lua_ls",
-      "rust_analyzer",
-      "sqlls",
-      "ts_ls",
-      "yamlls",
-    },
-  })
-
-  -- Order matters. lspconfig should load after mason-lspconfig
-  local lspconfig = require("lspconfig")
-
-  local style = require("my.styles")
-  require("lspconfig.ui.windows").default_options.border = style.border
-
-  -- Fix float border highlight group for LspInfo
-  vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "FloatBorder" })
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
   local function on_attach(client, bufnr)
     require("my.mappings").lsp_mapping(bufnr)
   end
 
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-  masonLspconfig.setup_handlers({
-    -- The first entry (without a key) will be the default handler and will be
-    -- called for each installed server that doesn't have a dedicated handler.
-    function(server_name)
-      lspconfig[server_name].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end,
-    -- Server customizations
-    ["rust_analyzer"] = function()
-      lspconfig["rust_analyzer"].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          ["rust-analyzer"] = {
-            checkOnSave = true,
-            check = { command = "clippy" },
-            cargo = { features = "all" },
-          },
-        },
-      })
-    end,
-    ["basedpyright"] = function()
-      lspconfig["basedpyright"].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          basedpyright = {
-            analysis = { typeCheckingMode = "off" },
-          },
-        },
-      })
-    end,
+  vim.lsp.config("*", {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+  vim.lsp.config("rust_analyzer", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = true,
+        check = { command = "clippy" },
+        cargo = { features = "all" },
+      },
+    },
+  })
+  vim.lsp.config("basedpyright", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+      basedpyright = {
+        analysis = { typeCheckingMode = "off" },
+      },
+    },
   })
 
   local signs = {
@@ -96,7 +55,11 @@ function P.config()
     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
   end
 
-  local border = require("my.styles").border
+  local style = require("my.styles")
+  require("lspconfig.ui.windows").default_options.border = style.border
+
+  -- Fix float border highlight group for LspInfo
+  vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "FloatBorder" })
 
   vim.diagnostic.config({
     virtual_text = false,
@@ -110,7 +73,7 @@ function P.config()
       focusable = false,
       style = "minimal",
       source = true,
-      border = border,
+      border = style.border,
       header = "",
       prefix = "",
     },
