6115c24c6736ec24b48765f546c5cfc5dbe52e0d
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-04-07 15:06:05 -0400

    nvim: Switch to catgoose/nvim-colorizer.lua
    norcalli/nvim-colorizer.lua is unmaintained and uses deprecated
    vim.tbl_flatten on neovim 0.12.

--- base/nvim/lua/my/plugins/nvim-colorizer.lua
+++ base/nvim/lua/my/plugins/nvim-colorizer.lua
@@ -2,18 +2,16 @@
 
 ---@type LazySpec
 local P = {
-  "norcalli/nvim-colorizer.lua",
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = {
+    filetypes = { "*" },
+    options = {
+      parsers = {
+        names = { enable = false },
+      },
+    },
+  },
 }
-
-function P.config()
-  local colorizer = require("colorizer")
-
-  colorizer.setup({ "*" }, {
-    RRGGBB = true,
-    RGB = false,
-    names = false,
-    RRGGBBAA = false,
-  })
-end
 
 return P
