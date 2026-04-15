322bd76ac8bc4f6101c47b4c41d168d390816cb0
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-04-15 17:15:50 -0400

    nvim: Upgrade treesitter

--- base/nvim/lua/my/plugins/nvim-treesitter.lua
+++ base/nvim/lua/my/plugins/nvim-treesitter.lua
@@ -4,8 +4,8 @@
 local P = {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  event = "BufRead",
   cmd = {
     "TSInstall",
     "TSInstallInfo",
@@ -13,62 +13,36 @@ local P = {
     "TSUninstall",
     "TSUpdate",
     "TSUpdateSync",
-    "TSDisableAll",
-    "TSEnableAll",
   },
 }
 
-function P.config()
-  local treesitter = require("nvim-treesitter.configs")
+local languages = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "dockerfile",
+  "go",
+  "html",
+  "javascript",
+  "jq",
+  "json",
+  "jsonnet",
+  "lua",
+  "markdown",
+  "php",
+  "python",
+  "regex",
+  "rust",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+}
 
-  treesitter.setup({
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "css",
-      "dockerfile",
-      "go",
-      "html",
-      "javascript",
-      "jq",
-      "json",
-      "jsonnet",
-      "lua",
-      "markdown",
-      "php",
-      "python",
-      "regex",
-      "rust",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-    },
-    sync_install = false,
-    ignore_install = {},
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    autopairs = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-    rainbow = {
-      enable = true,
-      disable = { "html" },
-      extended_mode = false,
-      max_file_lines = nil,
-    },
-  })
+function P.config()
+  require("nvim-treesitter").install(languages)
 end
 
 return P
